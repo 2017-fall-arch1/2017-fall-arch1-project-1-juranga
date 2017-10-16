38ee458e059c802b02bd23674a84b8d05fa68971
student <student@localhost.localdomain>
2017-10-15 20:26:24 -0600

    Final update to project.

---
+++ bTreeHW/README.md
@@ -0,0 +1,16 @@
+#Code Created by Gerardo Uranga for Computer Arch Lab 1
+
+This directory contains:
+1) Code that implements a BTree in bTree.c
+2) An employees.txt file to populate the tree at the start
+3) A demo program that uses it
+
+To compile:
+$make
+
+To test it:
+$make demo
+
+to delete binaries:
+$make clean
+
---
+++ bTreeHW/bTree.h
@@ -0,0 +1,44 @@
+#ifndef bTree_included
+#define bTree_included
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Node{
+  struct Node *left;
+  struct Node *right;
+  char *name;
+}node;
+
+/*Creates a Node for the Tree with the name specified*/
+node *createNode(char name[]);
+
+/* ASCII Comparison between the first and second strings.
+   Returns 2 if First is equal to Second.
+   Returns 1 if First is Greater.
+   Returns 0 if First is Smaller. */
+int compare (char first[], char second[]);
+
+/*Adds a name to the Tree */
+void addName(node **h, char *name);
+
+/*Populates Tree from a Text File */
+void populateFromFile(node **h, char *name);
+
+/* Writes Tree Names to a file. */
+void writeToFile(node *h, FILE **file);
+
+/* Lists all the names in the file from smallest to largest. */
+void listNames(node *h);
+
+/*Gets the smallest node from the right side of the tree to replace removed node*/
+node **getSmallestFromLargest(node **head);
+
+/*Gets the largest node from the left side of the tree to replace removed node */
+node **getLargestFromSmallest(node **head);
+
+/* Removes a name from a tree. */
+void removeName(node **head, char name[]);
+
+#endif
